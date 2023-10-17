6b1fb23b7d363aeb616ba9fd36b8e0f55f34f3d9
Brodie <brodiemkruk@gmail.com>
2023-10-16 21:52:42 -0400

    lab 4

---
+++ lab-4/script.js
@@ -0,0 +1,18 @@
+let firstName =  "brodie"
+let lastName = "kruk"
+let fullName =  firstName + " " + lastName
+console.log(fullName)
+
+let population = 632464
+console.log(population)
+console.log(population*3)
+
+const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+for (let i=1; i <= 5; i++){
+    console.log(i);
+}
+
+for (let i=0; i <= 4; i++){
+    console.log(i);
+}
