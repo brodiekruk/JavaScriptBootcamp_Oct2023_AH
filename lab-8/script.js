dade5ba70085fc36789b19c93c251e56ef5806ca
Brodie <brodiemkruk@gmail.com>
2023-10-26 18:37:52 -0400

    labs 8 and 9

---
+++ lab-8/script.js
@@ -0,0 +1,15 @@
+function getAreaOfCircle(radius) {
+    return ((radius*radius)*Math.PI)
+}
+
+function getACircumferenceOfCircle(radius) {
+    return ((2*Math.PI)*radius)
+}
+
+function getAreaOfSquare(side) {
+    return side*side
+}
+
+function getAreaOfTriangle(base, height) {
+    return (base*height)*0.5
+}
